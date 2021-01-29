0653012c3f8271105f2bc706a5373809c25f33c5
Omid <67732782+Omid-Mirzaee-Yazdi@users.noreply.github.com>
2021-01-29 10:40:48 +0330

    Update conn.js

--- src/conn.js
+++ src/conn.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyB0TpquNfGVL3xRo-fmUm_pwkSxXLh1vZM",
-        authDomain: "eggiechat.firebaseapp.com",
-        projectId: "eggiechat",
-        storageBucket: "eggiechat.appspot.com",
-        messagingSenderId: "796791645099",
-        appId: "1:796791645099:web:f1e1c23b56d57924e628ee"
-       
+       /* YOUR API KEYS */
 });
 const db=firebaseApp.firestore();
 export default db;
